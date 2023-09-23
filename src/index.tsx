4e0b3d572752750b81e49df2645b475f2cd1fa58
Okwoli-Godwin <okwolig60@gmail.com>
2023-09-23 13:14:49 +0100

    update

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+// import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { Provider } from 'react-redux/es/exports';
@@ -11,6 +11,7 @@ import { Store } from './Components/Apis/Store';
 import { persistStore } from 'redux-persist';
 import { PersistGate } from 'redux-persist/integration/react';
 import ScrollToTop from './Components/Scrolltotop';
+import { HashRouter} from 'react-router-dom';
 
 const queryClient = new QueryClient();
 let persistor = persistStore(Store);
@@ -23,10 +24,10 @@ root.render(
     <Provider store={Store}>
       <PersistGate persistor={persistor}>
         <QueryClientProvider client={queryClient}>
-          <BrowserRouter>
+          <HashRouter>
             <ScrollToTop />
             <App />
-          </BrowserRouter>
+          </HashRouter>
           <ReactQueryDevtools />
         </QueryClientProvider>
       </PersistGate>
